e632dd093ab2e808fb1a2635579eb05fe01bd0c5
박근석 <parkrootseok@gmail.com>
2025-12-13 20:08:55 +0900

    section12/13~15 수강

---
+++ section12/emotion-diary/src/components/Editor.jsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import Button from "./Button";
+import "./Editor.css";
+import EmotionItem from "./EmotionItem";
+import { data, useNavigate } from "react-router-dom";
+
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionName: "완전 좋음",
+  },
+  {
+    emotionId: 2,
+    emotionName: "좋음",
+  },
+  {
+    emotionId: 3,
+    emotionName: "그럭저럭",
+  },
+  {
+    emotionId: 4,
+    emotionName: "나쁨",
+  },
+  {
+    emotionId: 5,
+    emotionName: "끔찍함",
+  },
+];
+
+const getStringedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ diary, onSubmit }) => {
+  const nav = useNavigate();
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  useEffect(() => {
+    if (diary) {
+      setInput({ ...diary, createdDate: new Date(diary.createdDate) });
+    }
+  }, [diary]);
+
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
+
+  return (
+    <div className="Editor">
+      <section className="date_section">
+        <h4>오늘의 날짜</h4>
+        <input
+          name="createdDate"
+          value={getStringedDate(input.createdDate)}
+          type="date"
+          onChange={onChangeInput}
+        />
+      </section>
+      <section className="emotion_section">
+        <h4>오늘의 감정</h4>
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === input.emotionId}
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                })
+              }
+            />
+          ))}
+        </div>
+      </section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea
+          name="content"
+          value={input.content}
+          placeholder="오늘은 어땠나요?"
+          onChange={onChangeInput}
+        />
+      </section>
+      <section className="button_section">
+        <Button text={"취소하기"} onClick={() => nav(-1)} />
+        <Button
+          text={"작성완료"}
+          type={"POSITIVE"}
+          onClick={onClickSubmitButton}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Editor;
--- section12/emotion-diary/src/pages/Edit.jsx
+++ section12/emotion-diary/src/pages/Edit.jsx
@@ -1,9 +1,64 @@
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
+
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const Edit = () => {
+  const nav = useNavigate();
   const params = useParams();
+  const data = useContext(DiaryStateContext);
+  const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
+  const [diary, setDiary] = useState();
 
-  return <div>{params.id}</div>;
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", { replace: true });
+    }
+
+    setDiary(currentDiaryItem);
+  }, [params.id]);
+
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+    }
+
+    nav("/", { replace: true });
+  };
+
+  return (
+    <div>
+      {" "}
+      <Header
+        title={"일기 수정하기"}
+        leftchild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />}
+        rightchild={
+          <Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />
+        }
+      />
+      <Editor diary={diary} onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Edit;
